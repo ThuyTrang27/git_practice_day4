c3e636920372ba0f7586e64e96eebe0434c78e42
ThuyTrang27 <nguyenthuytrang2021bd@gmail.com>
2025-08-12 09:50:04 +0700

    next

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ console.log(a);
 console.log("In the morning");
 console.log("In the afternoon")
 console.log("In the evening")
-console.log("Dinner, Lunch")
+console.log("Breakfast, Dinner, Lunch")
+console.log("Today")
+var b = 4; c=5
+console.log(b+c);
